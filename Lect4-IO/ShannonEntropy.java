ac35586c04f843049c928f0dfdc6084960b26d85
cool4zbl <i@zhangbinliu.me>
2019-10-25 11:23:49 +0800

    use `double` type for count to fix NaN

--- Lect4-IO/ShannonEntropy.java
+++ Lect4-IO/ShannonEntropy.java
@@ -8,7 +8,7 @@ public class ShannonEntropy {
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]);
         int[] a = new int[m + 1];
-        int count = 0;
+        double count = 0;
         double entropy = 0.0;
 
         while (!StdIn.isEmpty()) {
