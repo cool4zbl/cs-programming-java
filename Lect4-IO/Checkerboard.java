49d145c37d3d5b5a30b2405d2fc07c80ffb418d2
cool4zbl <i@zhangbinliu.me>
2019-10-25 10:44:51 +0800

    update IO Checkerboard

--- Lect4-IO/Checkerboard.java
+++ Lect4-IO/Checkerboard.java
@@ -1,35 +1,35 @@
 /* *****************************************************************************
  *  Name:              Alan Turing
  *  Coursera User ID:  123456
- *  Last modified:     10/22/2019
+ *  Last modified:     10/25/2019
  **************************************************************************** */
 
 public class Checkerboard {
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        double[] x = new double[N + 1];
-        double[] y = new double[N + 1];
+        int n = Integer.parseInt(args[0]);
+        double[] verticesX = new double[n];
+        double[] verticesY = new double[n];
 
-        double scale = 100;
-        StdDraw.setScale(0, scale);
+        double scale = n;
+        StdDraw.setScale(0, n);
 
-        for (int i = 0; i <= N; i++) {
-            x[i] = scale * i / N;
-            y[i] = scale * i / N;
+        double r = scale / (2 * n);
+
+        for (int i = 0; i < n; i++) {
+            verticesX[i] = scale * i / n + r;
+            verticesY[i] = scale * i / n + r;
         }
 
-        for (int i = 0; i <= N; i++) {
-            for (int j = 0; j <= N; j++) {
-                if (j % 2 == 0 && i % 2 != 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || Math.abs(i - j) % 2 == 0) {
                     StdDraw.setPenColor(StdDraw.BLUE);
                 }
                 else {
                     StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
                 }
-                StdOut.println(i + " " + x[i] + ", " + y[j]);
-                StdDraw.filledSquare(x[i], y[j], scale / N * 0.5);
+                StdDraw.filledSquare(verticesX[i], verticesY[j], r);
             }
         }
-
     }
 }
