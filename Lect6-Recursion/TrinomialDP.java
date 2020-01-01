8729461065ae6b97f3b9d1bb40674dc54c3db565
cool4zbl <i@zhangbinliu.me>
2020-01-01 17:52:09 +0800

    TrinomialDP is amazing

--- Lect6-Recursion/TrinomialDP.java
+++ Lect6-Recursion/TrinomialDP.java
@@ -1,30 +1,29 @@
 /* *****************************************************************************
  *  Name:              Alan Turing
  *  Coursera User ID:  123456
- *  Last modified:     12/30/2019
+ *  Last modified:     1/1/2020
  **************************************************************************** */
 
 public class TrinomialDP {
     public static long trinomial(int n, int k) {
-        long[][] tri = new long[n + 3][k + 3];
+        k = Math.abs(k);
+
+        long[][] tri = new long[n + 2][k + n + 1];
 
         tri[0][0] = 1;
         tri[1][0] = 1;
 
-        for (int j = 1; j <= k; j++) tri[0][j] = 0;
-
-        // Why `n+1`?
-        for (int i = 1; i <= n + 1; i++) {
-            for (int j = 0; j <= k + 1; j++) {
-                if (j == 0) {
-                    tri[i][j] = tri[i - 1][1] + tri[i - 1][0] + tri[i - 1][1];
-                }
-                else if (j > i) {
+        for (int i = 1; i <= n; i++) {
+            // k 的循环次数，由 k & n 共同决定.
+            // T(n, k) = T(n - 1, Math.abs(k) - 1) + T(n - 1, Math.abs(k)) + T(n - 1, Math.abs(k) + 1)
+            // 因为层层递归，每次需要算到 k + n - i 的位置.
+            for (int j = 0; j <= k + (n - i); j++) {
+                if (j > i) {
                     tri[i][j] = 0;
+                    continue;
                 }
-                else {
-                    tri[i][j] = tri[i - 1][j - 1] + tri[i - 1][j] + tri[i - 1][j + 1];
-                }
+
+                tri[i][j] = tri[i - 1][Math.abs(j - 1)] + tri[i - 1][j] + tri[i - 1][j + 1];
             }
         }
         return tri[n][k];
@@ -33,7 +32,6 @@ public class TrinomialDP {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int k = Integer.parseInt(args[1]);
-        if (k < 0) k = -k;
 
         StdOut.println(trinomial(n, k));
     }
