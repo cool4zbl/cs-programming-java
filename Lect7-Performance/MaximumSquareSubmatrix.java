42e021f3ffc7ba690ae3f418e6b1025eaf0b8364
cool4zbl <i@zhangbinliu.me>
2020-01-14 22:17:21 +0800

    Update `MaximumSquareSubmatrix` by using DP

--- Lect7-Performance/MaximumSquareSubmatrix.java
+++ Lect7-Performance/MaximumSquareSubmatrix.java
@@ -1,45 +1,101 @@
 /* *****************************************************************************
  *  Name:              Alan Turing
  *  Coursera User ID:  123456
- *  Last modified:     1/13/2019
+ *  Last modified:     1/14/2019
  **************************************************************************** */
 
 public class MaximumSquareSubmatrix {
     // Returns the size of the largest contiguous square submatrix
     // of a[][] containing only 1s.
-    public static int size(int[][] a) {
+
+    /**
+     * 第1种，dp 矩阵为 (n+1) * (n+1), 第一行 & 列为 base cases.
+     */
+    public static int size3(int[][] a) {
+        int n = a.length;
+        int[][] dp = new int[n + 1][n + 1];
+        int maxLen = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a[i - 1][j - 1] == 1) {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                    maxLen = Math.max(dp[i][j], maxLen);
+                }
+            }
+        }
+
+        printMatrix(dp);
+        return maxLen;
+    }
+
+    /**
+     * 第二种，dp 矩阵为 n * n
+     */
+    public static int size2(int[][] a) {
         int n = a.length;
         int[][] dp = new int[n][n];
+        int maxLen = 0;
 
         for (int i = 0; i < n; i++) {
             dp[i][0] = a[i][0];
         }
-
         for (int j = 0; j < n; j++) {
             dp[0][j] = a[0][j];
         }
-
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < n; j++) {
-                int max = dp[i - 1][j - 1];
-                if (a[i][j] + a[i - max][j] + a[i][j - max] + a[i - 1][j - 1] == 4) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else {
-                    // dp[i][j] = dp[i - 1][j - 1];
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (a[i][j] == 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
                 }
             }
         }
+        printMatrix(dp);
+        return maxLen;
+    }
 
+    /**
+     * DP 优化，使用一维数组
+     *
+     * @param a 矩阵
+     * @return maxLen
+     */
+    public static int size(int[][] a) {
+        int n = a.length;
+        int[] dp = new int[n + 1];
+        int maxLen = 0;
+        int prev = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (a[i - 1][j - 1] == 1) {
+                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
+                    maxLen = Math.max(dp[j], maxLen);
+                }
+                else {
+                    dp[j] = 0;
+                }
+                prev = temp;
+            }
+        }
+
+        for (int i = 0; i <= n; i++) {
+            StdOut.print(dp[i] + " ");
+        }
+
+        return maxLen;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        int n = matrix.length;
         for (int j = 0; j < n; j++) {
             for (int i = 0; i < n; i++) {
-                StdOut.print(dp[i][j] + " ");
+                StdOut.print(matrix[i][j] + " ");
             }
             StdOut.println();
         }
-
-        return dp[n - 1][n - 1];
     }
 
     // Reads an n-by-n matrix of 0s and 1s from standard input
@@ -47,7 +103,7 @@ public class MaximumSquareSubmatrix {
     // containing only 1s.
     public static void main(String[] args) {
         int value = StdIn.readInt();
-        int[][] a = new int[value + 1][value + 1];
+        int[][] a = new int[value][value];
 
         while (!StdIn.isEmpty()) {
             for (int j = 0; j < value; j++) {
